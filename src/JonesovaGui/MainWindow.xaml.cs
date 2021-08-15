98a2d6f051db9b8389ba31750c56bdabea04c23e
Jan Joneš <jjones@outlook.cz>
2021-08-15 13:16:39 +0200

    Clone or pull Git repository

--- src/JonesovaGui/MainWindow.xaml.cs
+++ src/JonesovaGui/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using LibGit2Sharp;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace JonesovaGui
 {
@@ -20,15 +13,103 @@ namespace JonesovaGui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string tokenPath;
+
         public MainWindow()
         {
+            tokenPath = Path.GetFullPath("jjonesova.cz/token.txt");
+
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // TODO: Check Git repo.
+            // Load token if saved.
+            if (File.Exists(tokenPath))
+                tokenBox.Text = File.ReadAllText(tokenPath);
+
+            // Check Git repo.
+            var repoPath = Path.GetFullPath("jjonesova.cz/repo");
+            Directory.CreateDirectory(repoPath);
+            try
+            {
+                if (!Repository.IsValid(repoPath))
+                {
+                    // Clone repository.
+                    Directory.Delete(repoPath, recursive: true);
+                    await Task.Run(() => Repository.Clone("https://github.com/jjonescz/jjonesova", repoPath, new CloneOptions
+                    {
+                        CredentialsProvider = Credentials,
+                        OnProgress = p => Status("Přihlašování", p),
+                        OnTransferProgress = p => Status("Přihlašování", p),
+                        OnCheckoutProgress = (p, c, t) => Status("Přihlašování", $"Checkout: {(double)c / t:p}")
+                    }));
+                }
+                else
+                {
+                    // Pull repository.
+                    using var repo = new Repository(repoPath);
+                    var signature = new Signature("Jan Joneš", "jjones@outlook.cz", DateTimeOffset.Now);
+                    var result = await Task.Run(() => Commands.Pull(repo, signature, new PullOptions
+                    {
+                        FetchOptions = new FetchOptions
+                        {
+                            CredentialsProvider = Credentials,
+                            OnProgress = p => Status("Přihlašování", p),
+                            OnTransferProgress = p => Status("Přihlašování", p),
+                        }
+                    }));
+                    if (result.Status == MergeStatus.Conflicts)
+                    {
+                        loginStatus.Content = "Konflikt";
+                        loginStatus.Foreground = Brushes.DarkRed;
+                        tokenBox.Visibility = Visibility.Collapsed;
+                        return;
+                    }
+                }
+            }
+            catch (LibGit2SharpException)
+            {
+                loginStatus.Content = "Chyba; zadejte kód:";
+                loginStatus.Foreground = Brushes.DarkRed;
+                tokenBox.Visibility = Visibility.Visible;
+                return;
+            }
+
             loginStatus.Content = "Přihlášení úspěšné";
+            loginStatus.Foreground = Brushes.Black;
+            tokenBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void tokenBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Save token.
+            File.WriteAllText(tokenPath, tokenBox.Text);
+        }
+
+        private bool Status(string title, string progress)
+        {
+            _ = Dispatcher.InvokeAsync(() =>
+            {
+                loginStatus.Content = $"{title}: {progress}";
+                loginStatus.Foreground = Brushes.DarkOrange;
+                tokenBox.Visibility = Visibility.Collapsed;
+            });
+            return true;
+        }
+
+        private bool Status(string title, TransferProgress p)
+        {
+            return Status(title, $"Transfer: {(double)p.ReceivedObjects / p.TotalObjects:p}");
+        }
+
+        private Credentials Credentials(string url, string usernameFromUrl, SupportedCredentialTypes types)
+        {
+            return new UsernamePasswordCredentials
+            {
+                Username = "jjonescz",
+                Password = Dispatcher.Invoke(() => tokenBox.Text)
+            };
         }
     }
 }
