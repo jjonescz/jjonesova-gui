6b30799ccb16c1f7a33966fbc4b530210b4d786a
Jan Joneš <jjones@outlook.cz>
2021-08-15 18:00:48 +0200

    Refactor Git functionality into separate class

---
+++ src/JonesovaGui/Git.cs
@@ -0,0 +1,104 @@
+﻿using LibGit2Sharp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace JonesovaGui
+{
+    public partial class MainWindow
+    {
+        class Git
+        {
+            private readonly MainWindow window;
+
+            public Git(MainWindow window)
+            {
+                this.window = window;
+
+                GlobalSettings.LogConfiguration = new LogConfiguration(LogLevel.Trace,
+                    (level, message) => Log.Write(level, "Git", message));
+            }
+
+            public async Task RefreshAsync()
+            {
+                Directory.CreateDirectory(window.repoPath);
+                try
+                {
+                    if (!Repository.IsValid(window.repoPath))
+                    {
+                        // Clone repository.
+                        Directory.Delete(window.repoPath, recursive: true);
+                        await Task.Run(() => Repository.Clone("https://github.com/jjonescz/jjonesova", window.repoPath, new CloneOptions
+                        {
+                            CredentialsProvider = Credentials,
+                            OnProgress = p => Status("Přihlašování", p),
+                            OnTransferProgress = p => Status("Přihlašování", p),
+                            OnCheckoutProgress = (p, c, t) => Status("Přihlašování", $"Checkout: {(double)c / t:p}"),
+                            RecurseSubmodules = true
+                        }));
+                    }
+                    else
+                    {
+                        // Pull repository.
+                        using var repo = new Repository(window.repoPath);
+                        var signature = new Signature("Jan Joneš", "jjones@outlook.cz", DateTimeOffset.Now);
+                        var result = await Task.Run(() => Commands.Pull(repo, signature, new PullOptions
+                        {
+                            FetchOptions = new FetchOptions
+                            {
+                                CredentialsProvider = Credentials,
+                                OnProgress = p => Status("Přihlašování", p),
+                                OnTransferProgress = p => Status("Přihlašování", p)
+                            }
+                        }));
+                        if (result.Status == MergeStatus.Conflicts)
+                        {
+                            window.loginStatus.Content = "Konflikt";
+                            window.loginStatus.Foreground = Brushes.DarkRed;
+                            window.tokenBox.Visibility = Visibility.Collapsed;
+                            return;
+                        }
+                    }
+                }
+                catch (LibGit2SharpException)
+                {
+                    window.loginStatus.Content = "Chyba; zadejte kód:";
+                    window.loginStatus.Foreground = Brushes.DarkRed;
+                    window.tokenBox.Visibility = Visibility.Visible;
+                    return;
+                }
+
+                window.loginStatus.Content = "Přihlášení úspěšné";
+                window.loginStatus.Foreground = Brushes.Black;
+                window.tokenBox.Visibility = Visibility.Collapsed;
+            }
+
+            private bool Status(string title, string progress)
+            {
+                _ = window.Dispatcher.InvokeAsync(() =>
+                {
+                    window.loginStatus.Content = $"{title}: {progress}";
+                    window.loginStatus.Foreground = Brushes.DarkOrange;
+                    window.tokenBox.Visibility = Visibility.Collapsed;
+                });
+                return true;
+            }
+
+            private bool Status(string title, TransferProgress p)
+            {
+                return Status(title, $"Transfer: {(double)p.ReceivedObjects / p.TotalObjects:p}");
+            }
+
+            private Credentials Credentials(string url, string usernameFromUrl, SupportedCredentialTypes types)
+            {
+                return new UsernamePasswordCredentials
+                {
+                    Username = "jjonescz",
+                    Password = window.Dispatcher.Invoke(() => window.tokenBox.Text)
+                };
+            }
+        }
+    }
+}
--- src/JonesovaGui/MainWindow.xaml.cs
+++ src/JonesovaGui/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
-﻿using LibGit2Sharp;
-using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace JonesovaGui
 {
@@ -25,64 +21,12 @@ namespace JonesovaGui
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            GlobalSettings.LogConfiguration = new LogConfiguration(LogLevel.Trace,
-                (level, message) => Log.Write(level, "Git", message));
-
-            // Load token if saved.
+            // Load Git token.
             if (File.Exists(tokenPath))
                 tokenBox.Text = File.ReadAllText(tokenPath);
 
-            // Check Git repo.
-            Directory.CreateDirectory(repoPath);
-            try
-            {
-                if (!Repository.IsValid(repoPath))
-                {
-                    // Clone repository.
-                    Directory.Delete(repoPath, recursive: true);
-                    await Task.Run(() => Repository.Clone("https://github.com/jjonescz/jjonesova", repoPath, new CloneOptions
-                    {
-                        CredentialsProvider = Credentials,
-                        OnProgress = p => Status("Přihlašování", p),
-                        OnTransferProgress = p => Status("Přihlašování", p),
-                        OnCheckoutProgress = (p, c, t) => Status("Přihlašování", $"Checkout: {(double)c / t:p}"),
-                        RecurseSubmodules = true
-                    }));
-                }
-                else
-                {
-                    // Pull repository.
-                    using var repo = new Repository(repoPath);
-                    var signature = new Signature("Jan Joneš", "jjones@outlook.cz", DateTimeOffset.Now);
-                    var result = await Task.Run(() => Commands.Pull(repo, signature, new PullOptions
-                    {
-                        FetchOptions = new FetchOptions
-                        {
-                            CredentialsProvider = Credentials,
-                            OnProgress = p => Status("Přihlašování", p),
-                            OnTransferProgress = p => Status("Přihlašování", p)
-                        }
-                    }));
-                    if (result.Status == MergeStatus.Conflicts)
-                    {
-                        loginStatus.Content = "Konflikt";
-                        loginStatus.Foreground = Brushes.DarkRed;
-                        tokenBox.Visibility = Visibility.Collapsed;
-                        return;
-                    }
-                }
-            }
-            catch (LibGit2SharpException)
-            {
-                loginStatus.Content = "Chyba; zadejte kód:";
-                loginStatus.Foreground = Brushes.DarkRed;
-                tokenBox.Visibility = Visibility.Visible;
-                return;
-            }
-
-            loginStatus.Content = "Přihlášení úspěšné";
-            loginStatus.Foreground = Brushes.Black;
-            tokenBox.Visibility = Visibility.Collapsed;
+            // Update Git repo.
+            await new Git(this).RefreshAsync();
 
             // Execute Hugo.
             new Hugo(this).Start();
@@ -90,33 +34,8 @@ namespace JonesovaGui
 
         private void tokenBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // Save token.
+            // Save Git token.
             File.WriteAllText(tokenPath, tokenBox.Text);
-        }
-
-        private bool Status(string title, string progress)
-        {
-            _ = Dispatcher.InvokeAsync(() =>
-            {
-                loginStatus.Content = $"{title}: {progress}";
-                loginStatus.Foreground = Brushes.DarkOrange;
-                tokenBox.Visibility = Visibility.Collapsed;
-            });
-            return true;
-        }
-
-        private bool Status(string title, TransferProgress p)
-        {
-            return Status(title, $"Transfer: {(double)p.ReceivedObjects / p.TotalObjects:p}");
-        }
-
-        private Credentials Credentials(string url, string usernameFromUrl, SupportedCredentialTypes types)
-        {
-            return new UsernamePasswordCredentials
-            {
-                Username = "jjonescz",
-                Password = Dispatcher.Invoke(() => tokenBox.Text)
-            };
         }
     }
 }
